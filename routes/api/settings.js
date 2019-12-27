4030b72f88b5b756d27887f3babed5e651835b08
isysforce <isysforce@gmail.com>
2019-12-27 14:30:32 +0700

    Update settings.js

--- routes/api/settings.js
+++ routes/api/settings.js
@@ -4,6 +4,7 @@ const auth = require("../auth");
 const userController = require("../../controllers/userController");
 
 const Counter = mongoose.model("Counter")
+const Customer = mongoose.model("Customer")
 
 router.get(
   "/database/counters",
@@ -15,7 +16,9 @@ router.get(
     Counter.find({}).then(result => {
       return res.json(result)
     }).catch(error => {
-      return res.status(500).json({ error })
+      return res.status(500).json({
+        error
+      })
     })
   }
 );
@@ -27,20 +30,66 @@ router.patch(
   userController.grantAccess("updateAny"),
   (req, res, next) => {
     console.log("SET DATABASE COUNTER SEQUENCE")
-    const { name, sequence } = req.body
-    console.log({ name, sequence })
+    const {
+      name,
+      sequence
+    } = req.body
+    console.log({
+      name,
+      sequence
+    })
     if (!name || typeof sequence !== "number" || sequence > 999999) {
       return res.sendStatus(400)
     }
 
-    Counter.findOneAndUpdate({ _id: name }, { sequence }, { upsert: true, new: true }).then(data => {
+    Counter.findOneAndUpdate({
+      _id: name
+    }, {
+      sequence
+    }, {
+      upsert: true,
+      new: true
+    }).then(data => {
       console.log("SET DATABASE COUNTER SEQUENCE: SUCCESS")
-      return res.json({ data, status: "set_counter_success" })
+      return res.json({
+        data,
+        status: "set_counter_success"
+      })
     }).catch(error => {
       return res.status(500).json({
         error
       })
     })
+  }
+);
+
+
+router.get(
+  "/database/info",
+  auth.required,
+  userController.getUser,
+  userController.grantAccess("readAny"),
+  (req, res, next) => {
+
+    Customer.aggregate([{
+        $group: {
+          _id: {
+            war: "$war"
+          },
+          count: {
+            $sum: 1
+          }
+        }
+      }
+
+    ]).then(result => {
+      return res.json(result.map(item => ({
+        war: item._id.war,
+        count: item.count
+      })))
+    }).catch(next)
+
+
   }
 );
 
